c4cee2be3050c7aedde724a602b0852fa66a7bd4
alice-iceberg <aliceblackwood123@gmail.com>
2021-05-18 14:29:57 +0900

    huno_total_ema_score: show timestamp and ema answers separately

--- ET_Dashboard/views.py
+++ ET_Dashboard/views.py
@@ -884,7 +884,8 @@ def huno_json_total_ema_score(request):
     for ema in db.get_filtered_data_records(db_campaign=db_campaign, from_timestamp=from_ts, till_timestamp=till_ts,
                                             db_user=db_participant, db_data_source=db_data_source):
         cells = str(bytes(ema.value), encoding='utf8').split(' ')
-        res['EMA'][int(cells[0])] = {'ema1': int(cells[2]), 'ema2': int(cells[3]), 'ema3': int(cells[4]),
+        res['EMA']['timestamp'] = [int(cells[0])]
+        res['EMA']['ANSWER'] = {'ema1': int(cells[2]), 'ema2': int(cells[3]), 'ema3': int(cells[4]),
                                      'ema4': int(cells[5]), 'ema5': int(cells[6]), 'ema6': int(cells[7]),
                                      'ema7': int(cells[8]), 'ema8': int(cells[9]), 'ema9': int(cells[10]),
                                      'ema10': int(cells[11]), 'ema11': int(cells[12]), 'ema12': int(cells[13]),
