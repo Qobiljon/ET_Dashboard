1d4bb7813ecc1a01c69fa11dd5353b6fb277a586
Qobiljon <qobiljon.toshnazarov@gmail.com>
2021-06-14 17:25:03 +0900

    dbmgmt api moves data from postgres to cassandra

--- ET_Dashboard/views.py
+++ ET_Dashboard/views.py
@@ -714,17 +714,17 @@ def handle_download_csv_api(request):
                 if 'user_id' in request.GET and utils.is_numeric(request.GET['user_id']):
                     db_participant_user = db.get_user(user_id=int(request.GET['user_id']))
                     if db_participant_user is not None:
-                        dump_file_path = db.dump_csv_data(db_campaign=db_campaign, db_user=db_participant_user)
+                        dump_file_path = db.dump_data(db_campaign=db_campaign, db_user=db_participant_user)
                     else:
                         return redirect(to='campaigns-list')
                 elif 'data_source_id' in request.GET and utils.is_numeric(request.GET['data_source_id']):
                     db_data_source = db.get_data_source(data_source_id=int(request.GET['data_source_id']))
                     if db_data_source is not None:
-                        dump_file_path = db.dump_csv_data(db_campaign=db_campaign, db_data_source=db_data_source)
+                        dump_file_path = db.dump_data(db_campaign=db_campaign, db_data_source=db_data_source)
                     else:
                         return redirect(to='campaigns-list')
                 else:
-                    dump_file_path = db.dump_csv_data(db_campaign=db_campaign)
+                    dump_file_path = db.dump_data(db_campaign=db_campaign)
 
                 filename = os.path.basename(dump_file_path)
                 chunk_size = 8192
@@ -787,6 +787,64 @@ def handle_download_dataset_api(request):
 
 @require_http_methods(['POST'])
 def handle_db_mgmt_api(request):
+    import psycopg2
+    from psycopg2 import extras as psycopg2_extras
+    conn = psycopg2.connect(
+        host='127.0.0.1',
+        database='easytrack_db',
+        user='postgres',
+        password='postgres'
+    )
+    cur = conn.cursor(cursor_factory=psycopg2_extras.DictCursor)
+    session = db.get_cassandra_session()
+
+    # 1. copy campaign
+    cur.execute('select * from "et"."campaign" where "id"=4;')
+    pg_campaign = cur.fetchone()
+    cs_creator_user = db.get_user(user_id=2)
+    cs_campaign = db.create_or_update_campaign(
+        db_creator_user=cs_creator_user,
+        name=pg_campaign['name'],
+        notes=pg_campaign['notes'],
+        configurations=pg_campaign['config_json'],
+        start_timestamp=pg_campaign['start_timestamp'],
+        end_timestamp=pg_campaign['end_timestamp']
+    )
+    print('1. campaign copied')
+
+    # prepare data source map
+    cur.execute('select * from "et"."data_source";')
+    pg_data_source_ids = {}
+    for pg_data_source in cur.fetchall():
+        pg_data_source_ids[pg_data_source['name']] = pg_data_source['id']
+    data_source_id_map = {}
+    for cs_data_source in db.get_all_data_sources():
+        data_source_id_map[pg_data_source_ids[cs_data_source.name]] = cs_data_source.id
+    print('   data sources prepared')
+
+    # 2. copy participants and data
+    cur.execute('select * from "stats"."campaign_participant_stats" where "campaign_id"=4;')
+    pg_stats = cur.fetchall()
+    cs_data_sources = {}
+    for pg_stat in pg_stats:
+        # 2.1. copy participant
+        cur.execute('select * from "et"."user" where "id"=%s;', (pg_stat['user_id'],))
+        pg_participant = cur.fetchone()
+        cs_participant = db.create_user(name=pg_participant['name'], email=pg_participant['email'], session_key=utils.md5(value=f'{pg_participant["email"]}{utils.now_us()}'))
+        print(f'   participant copied (name = {cs_participant.name})')
+        # 2.2. copy data
+        db.bind_participant_to_campaign(db_user=cs_participant, db_campaign=cs_campaign)
+        cur.execute(f'select * from "data"."{pg_campaign["id"]}-{pg_participant["id"]}" limit 1000;')
+        for pg_value in cur.fetchall():
+            cs_data_source_id = data_source_id_map[pg_value['data_source_id']]
+            if cs_data_source_id not in cs_data_sources:
+                cs_data_sources[cs_data_source_id] = db.get_data_source(data_source_id=cs_data_source_id)
+            db.store_data_record(db_user=cs_participant, db_campaign=cs_campaign, db_data_source=cs_data_sources[cs_data_source_id], timestamp=pg_value['timestamp'], value=bytes(pg_value['value']))
+        print(f'   user data copied (name = {cs_participant.name})')
+
+    print('done')
+    cur.close()
+    conn.close()
     return JsonResponse(data={'result': 'success'})
 
 
--- tools/db_mgr.py
+++ tools/db_mgr.py
@@ -306,10 +306,13 @@ def get_filtered_data_records(db_user, db_campaign, db_data_source, from_timesta
     return data_records
 
 
-def dump_data(db_campaign, db_user):
+def dump_data(db_campaign, db_user, db_data_source=None):
     file_path = utils.get_download_file_path(f'cmp{db_campaign.id}_usr{db_user.id}.bin.csv')
     # session.execute(f'copy "data"."cmp{db_campaign.id}_usr{db_user.id}" to %s with header = true;', (file_path,))
-    subprocess.run(['cqlsh', '-e', f"copy data.cmp{db_campaign.id}_usr{db_user.id} to \'{file_path}\' with header = true;"], stdout=PIPE, stderr=PIPE)
+    if db_data_source:
+        subprocess.run(['cqlsh', '-e', f"copy data.cmp{db_campaign.id}_usr{db_user.id} to \'{file_path}\' with header = true;"], stdout=PIPE, stderr=PIPE)
+    else:
+        subprocess.run(['cqlsh', '-e', f"copy data.cmp{db_campaign.id}_usr{db_user.id} to \'{file_path}\' with header = true;"], stdout=PIPE, stderr=PIPE)
     return file_path
 
 
